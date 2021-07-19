f79ea6698af988fb6ae520f2545b680399b83a32
parikshit097 <mparikshit1997@gmail.com>
2021-07-19 21:24:01 +0530

    My String

---
+++ src/org/strings/Example.java
@@ -0,0 +1,12 @@
+package org.strings;
+
+public class Example {
+	public static void main(String[] args) {
+		String s="Welcome to java class";
+		String s1="nisha";
+	
+		String replace= s.replace (" ","#");
+		System.out.println(replace);
+	}
+
+}
