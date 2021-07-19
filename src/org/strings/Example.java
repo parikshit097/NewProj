e587cf44f36d2c982690e4ecb150b358f0d8d147
parikshit097 <mparikshit1997@gmail.com>
2021-07-19 22:37:30 +0530

    A changed

--- src/org/strings/Example.java
+++ src/org/strings/Example.java
@@ -4,6 +4,7 @@ public class Example {
 	public static void main(String[] args) {
 		String s="Welcome to java class";
 		String s1="nisha";
+		String s2="Parik";
 	
 		String replace= s.replace (" ","#");
 		System.out.println(replace);
